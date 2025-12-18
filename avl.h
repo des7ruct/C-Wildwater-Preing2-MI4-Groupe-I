9d53e8f62ab783c7b4a7c2187a387448fcb0415f
anthonyancelin <antho.ancelin@gmail.com>
2025-12-18 18:33:40 +0100

    Update avl.h

--- avl.h
+++ avl.h
@@ -16,5 +16,12 @@ AVL *insert_AVL(AVL *noeud, char *cle, Usine *data);
 Usine *avl_recherche(AVL *noeud, char *cle);
 void liberer_avl(AVL *n);
 void parcoursInfixeInverseAvl(AVL *n, FILE *sortie, int mode);
+int hauteur(AVL *n);
+int max(int a, int b);
+char *dupliquer_chaine(char *a);
+AVL *nouveau_noeud(char *cle, Usine *data);
+AVL *rotation_droite(AVL *y);
+AVL *rotation_gauche(AVL *x);
+int balance(AVL *n);
 
 #endif
