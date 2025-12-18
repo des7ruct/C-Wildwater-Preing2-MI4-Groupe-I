2793f6333af7fc19eb38a238bbf0016452d8c6db
anthonyancelin <antho.ancelin@gmail.com>
2025-12-18 18:29:39 +0100

    Update usine.c

--- usine.c
+++ usine.c
@@ -3,24 +3,41 @@
 
 int chaine_vers_int(char *s) {
     int r = 0;
-    if (s == NULL) return 0;
+    if (s == NULL) {
+        return 0;
+    }
+
     for (int i = 0; s[i] >= '0' && s[i] <= '9'; i++) {
         r = r * 10 + (s[i] - '0');
     }
+
     return r;
 }
 
 float chaine_vers_float(char *s) {
-    if (s == NULL) return 0.0;
+    if (s == NULL) {
+        return 0.0;
+    }
 
-    float r = 0.0, div = 1.0;
+    float r = 0.0;
+    float div = 1.0;
     int dec = 0;
+
     for (int i = 0; s[i] != '\0'; i++) {
-        if (s[i] == '.') dec = 1;
+        if (s[i] == '.') {
+            dec = 1;
+        }
         else if (s[i] >= '0' && s[i] <= '9') {
             r = r * 10 + (s[i] - '0');
-            if (dec) div *= 10.0;
-        } else break;
+            if (dec) {
+                div *= 10.0;
+            }
+        }
+        else {
+            break;
+        }
     }
+
     return r / div;
 }
+
