82b93953dd5dc98ee29e47cbc0d5b804363f6bb1
anthonyancelin <antho.ancelin@gmail.com>
2025-12-18 18:26:18 +0100

    Create usine.c

---
+++ usine.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "usine.h"
+
+int chaine_vers_int(char *s) {
+    int r = 0;
+    if (s == NULL) return 0;
+    for (int i = 0; s[i] >= '0' && s[i] <= '9'; i++) {
+        r = r * 10 + (s[i] - '0');
+    }
+    return r;
+}
+
+float chaine_vers_float(char *s) {
+    if (s == NULL) return 0.0;
+
+    float r = 0.0, div = 1.0;
+    int dec = 0;
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == '.') dec = 1;
+        else if (s[i] >= '0' && s[i] <= '9') {
+            r = r * 10 + (s[i] - '0');
+            if (dec) div *= 10.0;
+        } else break;
+    }
+    return r / div;
+}
