954fb432cad43351a42b5294966c35ff6eec9714
des7ruct <canaxel05@gmail.com>
2025-12-21 18:25:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "histogramme.h"
 #include "leaks.h"
 #include "outils.h"
 
 int main(int argc, char *argv[]) {
+    clock_t debut, fin ;
+    double temps_cpu ;
+    debut = clock() ;
 
     /* ===================== MODE HISTOGRAMME ===================== */
     if (argc == 4 && strcmp(argv[1], "histo") == 0) {
@@ -19,6 +23,10 @@ int main(int argc, char *argv[]) {
 
             printf("Mode histogramme invalide.\n");
             printf("Utilisation : ./projet_final <csv> histo <max|src|real>\n");
+
+            fin = clock() ;
+            temps_cpu = ((double)(fin - debut)) * 1000.0 / CLOCKS_PER_SEC ;
+            printf("Temps écoulé : %.2f ms\n", temps_cpu);
             return 1;
         }
 
@@ -28,10 +36,17 @@ int main(int argc, char *argv[]) {
 
         if (res != 0) {
             printf("Erreur lors du traitement de l'histogramme (code %d)\n", res);
+
+            fin = clock() ;
+            temps_cpu = ((double)(fin - debut)) * 1000.0 / CLOCKS_PER_SEC ;
+            printf("Temps écoulé : %.2f ms\n", temps_cpu);
             return res;
         }
 
         printf("Histogramme (%s) terminé avec succès.\n", mode);
+        fin = clock() ;
+        temps_cpu = ((double)(fin - debut)) * 1000.0 / CLOCKS_PER_SEC ;
+        printf("Temps écoulé : %.2f ms\n", temps_cpu);
         return 0;
     }
 
@@ -47,10 +62,16 @@ int main(int argc, char *argv[]) {
         
         if (fuites < 0.0) {
             printf("%.2f\n", fuites);
+            fin = clock() ;
+            temps_cpu = ((double)(fin - debut)) * 1000.0 / CLOCKS_PER_SEC ;
+            printf("Temps écoulé : %.2f ms\n", temps_cpu);
             return 0;
         }
 
         printf("Fuites totales pour %s : %.2f\n", id_usine, fuites);
+        fin = clock() ;
+        temps_cpu = ((double)(fin - debut)) * 1000.0 / CLOCKS_PER_SEC ;
+        printf("Temps écoulé : %.2f ms\n", temps_cpu);
         return 0;
     }
 
@@ -58,6 +79,8 @@ int main(int argc, char *argv[]) {
     printf("Utilisation :\n");
     printf("  Histogrammes : ./projet_final <csv> histo <max|src|real>\n");
     printf("  Leaks        : ./projet_final <csv> leaks <id_usine>\n");
-
+    fin = clock() ;
+    temps_cpu = ((double)(fin - debut)) * 1000.0 / CLOCKS_PER_SEC ;
+    printf("Temps écoulé : %.2f ms\n", temps_cpu);
     return 1;
 }
