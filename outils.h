6bed2ceef174cd4e8a231d64510b22f38a9f4b35
anthonyancelin <antho.ancelin@gmail.com>
2025-12-21 14:33:44 +0100

    Update outils.h

--- outils.h
+++ outils.h
@@ -1,8 +1,10 @@
 #ifndef UTILS_H
 #define UTILS_H
+#include <time.h>
 
 int valeur_absente(char *a);
 char *trim(char *s);
 int decouper_ligne(char *ligne, char *colonnes[], int max);
+long temps_en_millisecondes(struct timespec debut, struct timespec fin);
 
 #endif
